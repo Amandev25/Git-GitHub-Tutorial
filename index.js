4847ca072ae7e7bf243b5ffd85e44254dea51c63
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-04 10:00:19 +0530

    new feature added

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function saymyName()
 {
     return `My name is ${name}`;
 }
+
+// In Branch Aman-feature
+// this is a new feature
+function new_feature()
+{
+    console.log("this is a new feature");
+    
+}
